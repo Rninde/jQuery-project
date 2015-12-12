f107876f60a5f3eb1c8af63bd598c49dbfbd8f86
Rninde <richard.ninde@gmail.com>
2015-12-12 13:59:55 -0700

    Completed Resize

--- js/jscript.js
+++ js/jscript.js
@@ -1,25 +1,44 @@
 $(document).ready(function(){
 
+	makeGrid(16);
 
-function makeGrid() {
-	for (i=0; i<16; i++) {
+});
+
+function makeGrid(size) {
+	for (i=0; i<size; i++) {
 		$('#container').append('<div class = "grid"></div>')
-	}
-	for (j=0; j<16; j++) {
+	};
+	for (j=0; j<size; j++) {
 		$('.grid').append('<div class = "vert"></div>')
-	}
-}
-
-makeGrid();
-$('.vert').hover(function(){
-	$(this).addClass('highlight')
-
+	};
+	var vertH = 800/size;
+	$('.vert').css({'height':vertH, 'width': vertH});
+	$('.vert').hover(function(){
+		$(this).addClass('highlight');
 });
 
+};
 
+$('#resize').on('click', function(){
+	var gridS = prompt('What size?');
+	$('.vert').remove();
+	$('.grid').remove();
+	makeGrid(gridS);
 
+})
 
+$('#rainbow').on('click', function(){
+	var gridS = prompt('What size?');
+	$('.vert').remove();
+	$('.grid').remove();
+	$('.highlight').css('background-color', 'white' /*function(){
+		var letters = '0123456789ABCDEF'.split('');
+		var color = '#';
+		for (var k=0; k<6; i++) {
+			color += letters[Math.floor(Math.random()*16)];
+		};
+		return color;
+	}*/);	
+	makeGrid(gridS);
 
-
-
-});
+})
