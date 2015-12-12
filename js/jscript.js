67f89b0141753651d4cae965e4c3d61de6800eae
Rninde <richard.ninde@gmail.com>
2015-12-12 14:25:47 -0700

    Completed Rainbow

--- js/jscript.js
+++ js/jscript.js
@@ -14,10 +14,32 @@ function makeGrid(size) {
 	var vertH = 800/size;
 	$('.vert').css({'height':vertH, 'width': vertH});
 	$('.vert').hover(function(){
-		$(this).addClass('highlight');
+		$(this).css('background-color', 'white');
 });
-
 };
+
+function colorGrid(size) {
+	for (i=0; i<size; i++) {
+		$('#container').append('<div class = "grid"></div>')
+	};
+	for (j=0; j<size; j++) {
+		$('.grid').append('<div class = "vert"></div>')
+	};
+	var vertH = 800/size;
+	$('.vert').css({'height':vertH, 'width': vertH});
+	$('.vert').hover(function(){
+		$(this).css('background-color', function(){
+			var letters = '0123456789ABCDEF'.split('');
+			var color = '#';
+			for (var k=0; k<6; k++) {
+				color += letters[Math.floor(Math.random()*16)];
+			};
+			return color;
+		});
+});
+};
+
+
 
 $('#resize').on('click', function(){
 	var gridS = prompt('What size?');
@@ -31,14 +53,6 @@ $('#rainbow').on('click', function(){
 	var gridS = prompt('What size?');
 	$('.vert').remove();
 	$('.grid').remove();
-	$('.highlight').css('background-color', 'white' /*function(){
-		var letters = '0123456789ABCDEF'.split('');
-		var color = '#';
-		for (var k=0; k<6; i++) {
-			color += letters[Math.floor(Math.random()*16)];
-		};
-		return color;
-	}*/);	
-	makeGrid(gridS);
+	colorGrid(gridS);
 
 })
